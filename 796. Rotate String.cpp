9a15c5d028513088c9b1e68cc1ae4f1bd891d2de
Vaibhav-Raktate <vaibhavraktate305@gmail.com>
2025-02-10 23:11:18 +0530

    Rotate String

---
+++ 796. Rotate String.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    public:
+        bool rotateString(string s, string goal)
+        {
+            if(s.length()!=goal.length())
+            return false;
+    
+            string res=s+s;
+            cout<<res<<endl;
+    
+            int idx=res.find(goal);
+    
+            if(idx!=string::npos)
+            return true;
+            else
+            return false;
+            // queue<char> q1,q2;
+            
+            // for(int i=0;i<s.size();i++)
+            // q1.push(s[i]);
+    
+            //  for(int i=0;i<goal.size();i++)
+            // q2.push(goal[i]);
+            // int k=goal.size()-1;
+    
+            // while(k!=0){
+            //     char ch=q2.front();
+            //     q2.pop();
+            //     q2.push(ch);
+            //     if(q1==q2)
+            //     return true;
+            //     k--;
+            // }
+            // return false;
+        }
+    };
