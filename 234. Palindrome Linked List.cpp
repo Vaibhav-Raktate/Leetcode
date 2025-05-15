9f93dc7bf0c200ef5c13bc512fbf9ca96348c90f
Vaibhav Raktate <131482260+Vaibhav-Raktate@users.noreply.github.com>
2025-05-16 01:21:06 +0530

    234. Palindrome Linked List.cpp

--- 234. Palindrome Linked List.cpp
+++ 234. Palindrome Linked List.cpp
@@ -1,61 +1,62 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution {
-    private static ListNode reverseLinkedList(ListNode head){
-        if(head == null || head.next == null) return head;
-        ListNode newhead = reverseLinkedList(head.next);
-        ListNode front = head.next;
-        front.next = head;
-        head.next = null;
+public:
+    ListNode* reverseLinkedList(ListNode* head){
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode* newhead = reverseLinkedList(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
         return newhead;
     }
 
-    
-    public boolean isPalindrome(ListNode head) {
-
-        if(head == null || head.next == null) return true;
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast.next != null && fast.next.next != null){
-            slow =slow.next;
-            fast = fast.next.next;
+    bool isPalindrome(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow =slow->next;
+            fast = fast->next;
         }
 
-        ListNode newhead = reverseLinkedList(slow.next);
-        ListNode first = head;
-        ListNode second  =newhead;
-        while(second != null){
-            if(first.val != second.val){
+        ListNode* newhead = reverseLinkedList(slow->next);
+        ListNode* first = head;
+        ListNode* second  =newhead;
+        while(second != nullptr){
+            if(first->val != second->val){
                 reverseLinkedList(newhead);
                 return false;
             }
-            first = first.next;
-            second = second.next;
+            first = first->next;
+            second = second->next;
         }
         reverseLinkedList(newhead);
         return true;
 
 
-        // Stack<Integer> st = new Stack<>();
-        // ListNode temp = head;
-        // while(temp != null){
-        //     st.push(temp.val);
-        //     temp =temp.next;
+
+
+        // stack<int> st;
+        // ListNode* temp = head;
+        // while(temp != nullptr){
+        //     st.push(temp->val);
+        //     temp =temp->next;
         // }
         // temp =head;
-        // while(temp != null){
-        //     if(st.peek() != temp.val) return false;
-        //     temp = temp.next;
+        // while(temp != nullptr){
+        //     if(st.top() != temp->val) return false;
+        //     temp = temp->next;
         //     st.pop();
         // }
         // return true;
     }
-}
+};
