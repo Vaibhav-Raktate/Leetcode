a939ee93bbc01864b8dd93b1893dba3c589441a6
Vaibhav Raktate <131482260+Vaibhav-Raktate@users.noreply.github.com>
2025-07-16 00:08:20 +0530

    3136. Valid Word.cpp

---
+++ 3136. Valid Word.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string word) {
+         if(word.length()<3){
+            return false;
+         }
+
+         bool hasvowel = false;
+         bool hasconsonant = false;
+
+         for(char ch: word){
+            if(isalpha(ch)){
+                ch = tolower(ch);
+                if(ch == 'a'|| ch=='e'||ch == 'i'|| ch=='o'|| ch=='u'){
+                    hasvowel = true;
+                }else{
+                    hasconsonant = true;
+                }
+            } else if(!isdigit(ch)){
+                return false;
+            }
+         }
+         return hasvowel && hasconsonant;
+    }
+};
