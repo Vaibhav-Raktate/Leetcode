88b7cd7cd7144406031ae2c53b7fbfeeb7a6415e
Vaibhav Raktate <131482260+Vaibhav-Raktate@users.noreply.github.com>
2025-08-07 22:53:44 +0530

    3477. Fruits Into Baskets II.java
    Java Code

---
+++ 3477. Fruits Into Baskets II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        int unplaced = 0;
+
+        for(int i=0; i<n; i++){
+            boolean placed = false;
+            for(int j=0; j<n; j++){
+                if(fruits[i] <= baskets[j]){
+                    placed = true;
+                    baskets[j] = -1;
+                    break;
+                }
+            }
+            if(!placed){
+                unplaced++;
+            }
+        }
+        return unplaced;
+    }
+}
