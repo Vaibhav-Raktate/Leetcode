273d11a7af51bac7b13db409ebbf0e1262868246
Vaibhav Raktate <131482260+Vaibhav-Raktate@users.noreply.github.com>
2025-09-10 13:35:10 +0530

    1317. Convert Integer to the Sum of Two No-Zero Integers.cpp

--- 1317. Convert Integer to the Sum of Two No-Zero Integers.cpp
+++ 1317. Convert Integer to the Sum of Two No-Zero Integers.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    bool check(int num){
-        while(num){
-            if(num%10==0){
-                return false;
-            }
-            num/=10;
-        }
-        return true;
-    }
+    // bool check(int num){
+    //     while(num){
+    //         if(num%10==0){
+    //             return false;
+    //         }
+    //         num/=10;
+    //     }
+    //     return true;
+    // }
 
     vector<int> getNoZeroIntegers(int n) {
-        for(int a=1;a<=n-1; a++){
-            int b = n-a;
+        int a=n;
+        int b=0;
+        int placevalue = 1;
 
-            if(check(a)&& check(b)){
-                return{a,b};
+        while(n>1){
+            int take = 1;
+
+            if(n%10 == 1){
+                take = 2;
             }
+
+            a = a-take*placevalue;
+            b = b+take*placevalue;
+
+            n = (n-take)/10;
+            placevalue *=10;
+            
         }
-        return {};
+
+        // for(int a=1;a<=n-1; a++){
+        //     int b = n-a;
+
+        //     if(check(a)&& check(b)){
+        //         return{a,b};
+        //     }
+        // }
+        // return {};
+        return{a,b};
     }
 };
