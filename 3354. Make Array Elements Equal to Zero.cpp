95c3215f7dd9535b050b42a97b550dc4a0d1b159
Vaibhav Raktate <131482260+Vaibhav-Raktate@users.noreply.github.com>
2025-11-07 00:19:57 +0530

    3354. Make Array Elements Equal to Zero
    Implement countValidSelections method to count valid selections in the array.

---
+++ 3354. Make Array Elements Equal to Zero.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        int n = nums.size();
+        int result = 0;
+        int curr  = 0;
+        int S = accumulate(begin(nums),end(nums),0);
+
+        for(int i=0; i<n; i++){
+            curr += nums[i];
+
+            int left = curr;
+            int right = S - left;
+
+            if(nums[i] != 0)
+                continue;
+
+            if(left == right)
+                result += 2;
+
+            if(abs(left  - right) == 1)
+                result += 1;
+        }
+        return result;
+    }
+};
