3261e9cbbafd674f25a87c2f055138588d5d5e4a
Vaibhav Raktate <131482260+Vaibhav-Raktate@users.noreply.github.com>
2025-11-23 23:19:33 +0530

    Implement solution for max sum divisible by three1262. Greatest Sum Divisible by Three.cpp

---
+++ 1262. Greatest Sum Divisible by Three.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        vector<vector<int>> t(n+1, vector<int>(3, INT_MIN));
+
+        t[n][0] = 0;
+        t[n][1] = INT_MIN;
+        t[n][2] = INT_MIN;
+
+        for(int i = n-1; i>=0; i--){
+            for(int remain = 0; remain<=2; remain++){
+                int newRemain = (remain + nums[i])%3;
+                int take = nums[i] + t[i+1][newRemain];
+
+                int skip = t[i+1][remain];
+                t[i][remain] = max(take, skip);
+            }
+        }
+        return t[0][0];
+    }
+};
