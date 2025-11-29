aca78936406699e239f0d04bd67fc566a4e03c70
Vaibhav Raktate <131482260+Vaibhav-Raktate@users.noreply.github.com>
2025-11-29 23:56:26 +0530

    Implement solution for minimum operations problem
    java

---
+++ 3512. Minimum Operations to Make Array Sum Divisible by K.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int sum = 0;
+        for(int x : nums){
+            sum += x;
+        }
+
+        int rem = sum % k ;
+        if(rem == 0) return 0;
+        return rem;
+    }
+}
